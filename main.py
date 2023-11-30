9ee0168c83e4ef6086bd5b9513077fd48479dcf1
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-30 14:57:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -171,69 +171,91 @@ async def vickprivate(client: Client, message: Message):
                
 
 @bot.on_message(
- (
-        filters.sticker
-        | filters.text
+    (
+        filters.text
+        | filters.sticker
     )
-    & ~filters.private
+    & filters.private
     & ~filters.bot,
 )
-async def vickstickerai(client: Client, message: Message):
+async def vickprivate(client: Client, message: Message):
 
-   chatdb = MongoClient(MONGO_URL)
-   chatai = chatdb["Word"]["WordDb"]   
+    chatdb = MongoClient(MONGO_URL)
+    chatai = chatdb["Word"]["WordDb"]
 
-   if not message.reply_to_message:
-       vickdb = MongoClient(MONGO_URL)
-       vick = vickdb["VickDb"]["Vick"] 
-       is_vick = vick.find_one({"chat_id": message.chat.id})
-       if not is_vick:
-           await bot.send_chat_action(message.chat.id, "typing")
-           K = []  
-           is_chat = chatai.find({"word": message.sticker.file_unique_id})      
-           k = chatai.find_one({"word": message.text})      
-           if k:           
-               for x in is_chat:
-                   K.append(x['text'])
-               hey = random.choice(K)
-               is_text = chatai.find_one({"text": hey})
-               Yo = is_text['check']
-               if Yo == "text":
-                   await message.reply_text(f"{hey}")
-               if not Yo == "text":
-                   await message.reply_sticker(f"{hey}")
-   
-   if message.reply_to_message:
-       vickdb = MongoClient(MONGO_URL)
-       vick = vickdb["VickDb"]["Vick"] 
-       is_vick = vick.find_one({"chat_id": message.chat.id})
-       getme = await bot.get_me()
-       bot_id = getme.id
-       if message.reply_to_message.from_user.id == bot_id: 
-           if not is_vick:                    
-               await bot.send_chat_action(message.chat.id, "typing")
-               K = []  
-               is_chat = chatai.find({"word": message.text})
-               k = chatai.find_one({"word": message.text})      
-               if k:           
-                   for x in is_chat:
-                       K.append(x['text'])
-                   hey = random.choice(K)
-                   is_text = chatai.find_one({"text": hey})
-                   Yo = is_text['check']
-                   if Yo == "text":
-                       await message.reply_text(f"{hey}")
-                   if not Yo == "text":
-                       await message.reply_sticker(f"{hey}")
-       if not message.reply_to_message.from_user.id == bot_id:          
-           if message.text:
-               is_chat = chatai.find_one({"word": message.reply_to_message.sticker.file_unique_id, "text": message.text})
-               if not is_chat:
-                   toggle.insert_one({"word": message.reply_to_message.sticker.file_unique_id, "text": message.text, "check": "text"})
-           if message.sticker:                 
-               is_chat = chatai.find_one({"word": message.reply_to_message.sticker.file_unique_id, "text": message.sticker.file_id})                 
-               if not is_chat:
-                   chatai.insert_one({"word": message.reply_to_message.sticker.file_unique_id, "text": message.sticker.file_id, "check": "none"})    
+    if not message.reply_to_message:
+        await bot.send_chat_action(message.chat.id, "typing")
+        K = []
+        is_chat = chatai.find({"word": message.text})
+        for x in is_chat:
+            K.append(x['text'])
+        if not K:
+            return await message.reply_text("No responses available.")
+
+        hey = random.choice(K)
+        is_text = chatai.find_one({"text": hey})
+        Yo = is_text['check']
+        if Yo == "sticker":
+            await message.reply_sticker(f"{hey}")
+        if not Yo == "sticker":
+            await message.reply_text(f"{hey}")
+
+    if message.reply_to_message:
+        getme = await bot.get_me()
+        bot_id = getme.id
+        if message.reply_to_message.from_user.id == bot_id:
+            await bot.send_chat_action(message.chat.id, "typing")
+            K = []
+            is_chat = chatai.find({"word": message.text})
+            for x in is_chat:
+                K.append(x['text'])
+            if not K:
+                return await message.reply_text("No responses available.")
+
+            hey = random.choice(K)
+            is_text = chatai.find_one({"text": hey})
+            Yo = is_text['check']
+            if Yo == "sticker":
+                await message.reply_sticker(f"{hey}")
+            if not Yo == "sticker":
+                await message.reply_text(f"{hey}")
+
+        # ... rest of your code ...
+
+        if message.reply_to_message:
+            vickdb = MongoClient(MONGO_URL)
+            vick = vickdb["VickDb"]["Vick"]
+            is_vick = vick.find_one({"chat_id": message.chat.id})
+            getme = await bot.get_me()
+            bot_id = getme.id
+            if message.reply_to_message.from_user.id == bot_id:
+                if not is_vick:
+                    await bot.send_chat_action(message.chat.id, "typing")
+                    K = []
+                    is_chat = chatai.find({"word": message.text})
+                    k = chatai.find_one({"word": message.text})
+                    if k:
+                        for x in is_chat:
+                            K.append(x['text'])
+                        hey = random.choice(K)
+                        is_text = chatai.find_one({"text": hey})
+                        Yo = is_text['check']
+                        if Yo == "sticker":
+                            await message.reply_sticker(f"{hey}")
+                        if not Yo == "sticker":
+                            await message.reply_text(f"{hey}")
+                if not message.reply_to_message.from_user.id == bot_id:
+                    if message.sticker:
+                        is_chat = chatai.find_one({"word": message.reply_to_message.text, "id": message.sticker.file_unique_id})
+                        if not is_chat:
+                            chatai.insert_one({"word": message.reply_to_message.text, "text": message.sticker.file_id, "check": "sticker", "id": message.sticker.file_unique_id})
+                    if message.text:
+                        is_chat = chatai.find_one({"word": message.reply_to_message.text, "text": message.text})
+                        if not is_chat:
+                            chatai.insert_one({"word": message.reply_to_message.text, "text": message.text, "check": "none"})
+
+    # ... rest of your code ...
+  
                
 
 
