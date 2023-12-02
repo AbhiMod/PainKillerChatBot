2497923a8e92c97f4c9490cd966aae16ae01d70e
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-02 13:57:24 +0530

    Create mongo.py

---
+++ mongo /mongo.py
@@ -0,0 +1,14 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from main import MONGO_URL
+
+from ..logging import LOGGER
+
+LOGGER(__name__).info("Connecting to your Mongo Database...")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Anon
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except:
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    exit()
