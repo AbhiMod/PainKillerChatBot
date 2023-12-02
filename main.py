b416fd84df5cb4b12eca255428079e2b528fc109
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-02 09:53:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,55 @@ async def cancelcmd(_, message):
     else:
         await message.reply_text("**No ongoing process!**")
         return
-#JoinVc
+# Command handler to get group status
+@client.on_message(
+    filters.command(["Groupinfo"], prefixes=["/", ".", "?", "-", "", "!"])
+    & filters.group
+)
+def group_status(client, message):
+    chat = message.chat  # Chat where the command was sent
+    status_text = f"Group ID: {chat.id}\n" \
+                  f"Title: {chat.title}\n" \
+                  f"Type: {chat.type}\n"
+                  
+    if chat.username:  # Not all groups have a username
+        status_text += f"Username: @{chat.username}"
+    else:
+        status_text += "Username: None"
+
+    message.reply_text(status_text)
+
+
+#########
+
+""" ***                                                                       
+────────────────────────────────────────────────────────────────────────
+─████████████────██████████████──████████──████████──████████──████████─
+─██░░░░░░░░████──██░░░░░░░░░░██──██░░░░██──██░░░░██──██░░░░██──██░░░░██─
+─██░░████░░░░██──██░░██████░░██──████░░██──██░░████──████░░██──██░░████─
+─██░░██──██░░██──██░░██──██░░██────██░░░░██░░░░██──────██░░░░██░░░░██───
+─██░░██──██░░██──██░░██████░░██────████░░░░░░████──────████░░░░░░████───
+─██░░██──██░░██──██░░░░░░░░░░██──────██░░░░░░██──────────██░░░░░░██─────
+─██░░██──██░░██──██░░██████░░██────████░░░░░░████──────████░░░░░░████───
+─██░░██──██░░██──██░░██──██░░██────██░░░░██░░░░██──────██░░░░██░░░░██───
+─██░░████░░░░██──██░░██──██░░██──████░░██──██░░████──████░░██──██░░████─
+─██░░░░░░░░████──██░░██──██░░██──██░░░░██──██░░░░██──██░░░░██──██░░░░██─
+─████████████────██████──██████──████████──████████──████████──████████─
+────────────────────────────────────────────────────────────────────────**"""
+
+
+
+####
+@app.on_message(filters.command('groupinfo') & filters.group)
+def group_info(client, message):
+    chat_id = message.chat.id
+    chat_info = app.get_chat(chat_id)
+    title = chat_info.title
+    description = chat_info.description
+    member_count = chat_info.members_count
+
+    info_text = f"Group Title: {title}\nDescription: {description}\nMembers Count: {member_count}"
+    message.reply_text(info_text)
 
 #help
 @client.on_message(
